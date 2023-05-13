35563e7805824bd6a646794e891bb66b0657fa55
Tanguy Serrand <tanguy.serrand01@gmail.com>
2023-05-13 20:02:14 +0200

    register page OK

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
     path: 'auth', loadChildren: () => import('./auth/auth.module')
       .then(m => m.AuthModule)
   },
+  {
+    path: 'register', loadChildren: () => import('./register/register.module')
+      .then(m => m.RegisterModule)
+  },
 
   { path: "**", component: ErrorComponent }
 ];

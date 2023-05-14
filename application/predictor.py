0ba5d42b463d712f80387a9f1f5ffd88b67d7551
leopnt <leopnt.dev@gmail.com>
2023-05-14 13:12:22 +0200

    add and patch technical-analysis to predictor

--- application/predictor.py
+++ application/predictor.py
@@ -3,6 +3,7 @@ import requests
 import datetime
 
 from newssentiment.production.news_predictor import NewsSentimentPredict
+from technicalanalysis.GetTriangleAscendant import TriangleAscendantData
 
 def main():
     symbol = 'AAPL'
@@ -23,8 +24,40 @@ def main():
     print(prediction)
 
     base_url = 'http://127.0.0.1:5001/api/v1'
+
     r = requests.post(
             base_url + '/predictions/news',
+            json=prediction)
+
+    print(r.text)
+
+    tad = TriangleAscendantData(symbol)
+
+    # patch datetime
+    target_date = tad.to_dict()['target_date']
+    # +24 hours by default
+    target_datetime = datetime.datetime.now() + datetime.timedelta(days=1)
+    if target_date:
+        target_datetime = target_date
+
+    # patch target_price
+    target_price = tad.to_dict()['target_price']
+    if not target_price:
+        target_price = 0.0
+
+    prediction = {
+            'datetime': date_now,
+            'delta_price_predicted': target_price,
+            'target_datetime': target_datetime.strftime("%Y-%m-%d %H:%M:%S.%f"),
+            'stock_symbol': symbol,
+            }
+
+    print(prediction)
+
+    base_url = 'http://127.0.0.1:5001/api/v1'
+
+    r = requests.post(
+            base_url + '/predictions/technical',
             json=prediction)
 
     print(r.text)
